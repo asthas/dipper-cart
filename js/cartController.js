e39c6469b91cae52601496612e6cdece005684ae
Astha Sharma <a@astha.me>
2016-01-18 19:33:46 +0530

    Added a price and quantity input for display

--- js/cartController.js
+++ js/cartController.js
@@ -1,13 +1,11 @@
-angular.module('app')
+angular.module('app', [])
 	.controller('CartController', function () {
 		var vm = this;
-
 		vm.qty = 1;
 		vm.value = parseInt(vm.value);
 		vm.total = function() {
 			var total = 0;
 			total += vm.qty * vm.value;
-
 			return total;
 		}
 	})
